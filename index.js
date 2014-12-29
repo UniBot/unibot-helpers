d01283fa15e5f993c6079e26e7aa49e100cebf65
tarlepp <tarmo.leppanen@kase.fi>
2014-12-29 21:16:57 +0200

    Added support for HTTPS

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * @type {exports}
  */
 var http = require('http');
+var https = require('https');
 
 module.exports = {
     /**
@@ -14,6 +15,28 @@ module.exports = {
      */
     download: function download(url, callback) {
         http.get(url, function get(res) {
+            var data = '';
+
+            res.on('data', function chunk(chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function end() {
+                callback(data);
+            });
+        }).on('error', function error() {
+            callback(null);
+        });
+    },
+
+    /**
+     * Helper function to download specified SSL url contents.
+     *
+     * @param   {string}    url         URL to download
+     * @param   {Function}  callback    Callback function
+     */
+    downloadSsl: function downloadSsl(url, callback) {
+        https.get(url, function get(res) {
             var data = '';
 
             res.on('data', function chunk(chunk) {
